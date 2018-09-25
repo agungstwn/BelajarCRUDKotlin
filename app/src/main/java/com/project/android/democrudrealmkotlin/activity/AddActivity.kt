f3184f36a1fb77686ab035a0b2a963b1887f0f05
Agung <agungstwn0@gmail.com>
2018-09-25 14:59:03 +0700

    bug fix delete update

--- app/src/main/java/com/project/android/democrudrealmkotlin/activity/AddActivity.kt
+++ app/src/main/java/com/project/android/democrudrealmkotlin/activity/AddActivity.kt
@@ -2,15 +2,33 @@ package com.project.android.democrudrealmkotlin.activity
 
 import android.os.Bundle
 import android.support.annotation.LayoutRes
+import android.view.View
+import android.widget.Button
+import butterknife.BindView
+import butterknife.OnClick
 import com.project.android.democrudrealmkotlin.R
+import com.project.android.democrudrealmkotlin.bus.DataEvent
+import com.project.android.democrudrealmkotlin.model.DataModel
+import com.project.android.democrudrealmkotlin.realm.RealmHelper
+import com.project.android.democrudrealmkotlin.utils.Constants
+import com.rengwuxian.materialedittext.MaterialEditText
+import com.squareup.otto.Bus
+import javax.inject.Inject
 
 class AddActivity : BaseActivity() {
+
 
     @LayoutRes
     override fun getLayoutResId(): Int {
         return R.layout.activity_add
-
     }
+
+    @BindView(R.id.et_title) lateinit var mEtTitle: MaterialEditText
+    @BindView(R.id.et_content) lateinit var mEtContent: MaterialEditText
+    @BindView(R.id.btn_delete) lateinit var mBtnDelete: Button
+
+    @Inject lateinit var dataRealmManager: RealmHelper
+    @Inject lateinit var bus: Bus
 
     var noteId = -1L
 
@@ -18,5 +36,47 @@ class AddActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         appComponent().inject(this)
 
+        val bundle = intent.extras
+
+        if (bundle != null){
+            noteId = bundle.getLong(Constants.INTENT_KEY_NOTE_ID)
+        }
+
+        if (noteId == -1L){
+            supportActionBar?.title = "Create New"
+            mBtnDelete.visibility = View.GONE
+        }else{
+            supportActionBar?.title = "Edit"
+            mBtnDelete.visibility = View.VISIBLE
+            var dataModel = dataRealmManager.find(noteId)
+            mEtTitle.setText(dataModel?.title)
+            mEtContent.setText(dataModel?.content)
+        }
+    }
+
+    @OnClick(R.id.btn_save_update)
+    fun onClickBtnSave(){
+        if (noteId == -1L){
+            dataRealmManager.insert(
+                    mEtTitle.text.toString(),
+                    mEtContent.text.toString()
+            )
+            bus.post(DataEvent(DataEvent.ACTION_INSERT))
+        }else{
+            dataRealmManager.update(noteId,
+                    mEtTitle.getText().toString(),
+                    mEtContent.getText().toString()
+            )
+            bus.post(DataEvent(DataEvent.ACTION_UPDATE))
+        }
+        finish()
+    }
+
+    @OnClick(R.id.btn_delete)
+    fun onClickBtnDelete(){
+        dataRealmManager.delete(noteId)
+        bus.post(DataEvent(DataEvent.ACTION_DELETE))
+
+        finish()
     }
 }
--- app/src/main/java/com/project/android/democrudrealmkotlin/bus/DataEvent.kt
+++ app/src/main/java/com/project/android/democrudrealmkotlin/bus/DataEvent.kt
@@ -1,17 +1,23 @@
 package com.project.android.democrudrealmkotlin.bus
 
 import android.support.annotation.IntDef
+import android.support.annotation.LongDef
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
 
 /**
  * Created by agung on 21/09/18.
  */
 class DataEvent(@param:Query val query: Int) {
 
-    @IntDef(ACTION_INSERT, ACTION_UPDATE, ACTION_DELETE)
-    @Retention(AnnotationRetention.SOURCE)
+    @LongDef (ACTION_INSERT.toLong(), ACTION_UPDATE.toLong(), ACTION_DELETE.toLong())
+
+    @Retention(RetentionPolicy.SOURCE)
+
     annotation class Query
 
     companion object {
+
         const val ACTION_INSERT = 0
         const val ACTION_UPDATE = 1
         const val ACTION_DELETE = 2
